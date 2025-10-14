6db04baf15e3ef1ce1cf346c90e98c9936de434e
JonasGreiwe <j.greiwe@umweltlabor-acb.de>
2025-10-14 14:33:40 +0200

    .

--- time_tracking/urls.py
+++ time_tracking/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from .views import ScanView, BookingView
 
-app_name = 'time_tracking'
-
 urlpatterns = [
     path('', ScanView.as_view(), name='scan'),
-    path('booking/', BookingView.as_view(), name='booking'),
+    path('booking/', BookingView.as_view(), name='booking'),  # ← Muss so heißen!
 ]
