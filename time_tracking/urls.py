f0210108e5343b5eee5deb31ee961443c9f477e6
JonasGreiwe <j.greiwe@umweltlabor-acb.de>
2025-10-28 11:59:57 +0100

    .

--- time_tracking/urls.py
+++ time_tracking/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ScanView, BookingView, CheckChipView
+from .views import ScanView, BookingView, check_new_chip
 from time_tracking import views
 
 urlpatterns = [
