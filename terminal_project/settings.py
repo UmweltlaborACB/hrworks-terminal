7749bdb353c3a9215059ebebd6fb2c33855dc990
JonasGreiwe <j.greiwe@umweltlabor-acb.de>
2025-11-10 12:57:22 +0100

    .

--- terminal_project/settings.py
+++ terminal_project/settings.py
@@ -71,8 +71,8 @@ TIME_ZONE = 'Europe/Berlin'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = '/staticfiles/'
-STATIC_ROOT = BASE_DIR /  'staticfiles'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR /  'static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
