a3f9225d307d463b199a0d08c8a73e90a6800734
JonasGreiwe <j.greiwe@umweltlabor-acb.de>
2025-11-10 12:00:23 +0100

    .

--- terminal_project/settings.py
+++ terminal_project/settings.py
@@ -72,7 +72,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'time_tracking' / 'static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
